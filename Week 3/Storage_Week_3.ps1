0f0dd6897ae15fa0815ab9a55e5818caaeb556e0
CoenWilbrink <74359740+CoenWilbrink@users.noreply.github.com>
2022-06-01 22:36:36 +0200

    Add files via upload

--- Week 3/Storage_Week_3.ps1
+++ Week 3/Storage_Week_3.ps1
@@ -6,20 +6,20 @@ $shareName='myshare'
 $tableName='table01'
 $ContainerName='quickstartblobs'
 
-$storageAccount = New-AzStorageAccount -ResourceGroupName $ResourceGroupName \
-    -Name $storageAccountName \
-    -Location $region \
-    -SkuName Standard LRS \
+$storageAccount = New-AzStorageAccount -ResourceGroupName $ResourceGroupName `
+    -Name $storageAccountName `
+    -Location $region `
+    -SkuName Standard_LRS `
     -Kind StorageV2
 
 $ctx = $storageAccount.Context
 
 New-AzStorageContainer -Name $ContainerName -Context $ctx -Permission Blob
 
-New-AzRmStorageShare \
-    -StorageAccount $storageAccount \
-    -Name $shareName \
-    -EnabledProtocol SMB \
+New-AzRmStorageShare `
+    -StorageAccount $storageAccount `
+    -Name $shareName `
+    -EnabledProtocol SMB `
     -QuotaGiB 1024 | Out-Null
 
 New-AzStorageTable -Name $tableName -Context $ctx
